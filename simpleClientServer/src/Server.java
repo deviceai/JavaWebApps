34f3852b22f95802bb0ec9f68fba3f4e229c218e
deviceai <device.ai@gmail.com>
2019-12-02 22:41:47 +0200

    very simple socket server 2

--- simpleClientServer/src/Server.java
+++ simpleClientServer/src/Server.java
@@ -5,8 +5,17 @@ public class Server {
     public static void main(String[] args) throws IOException{
 
         ServerSocket serverSocket = new ServerSocket(4000);
-        serverSocket.accept();
-        serverSocket.close();
+        int count = 0;
+        while(true) {
+            Socket clientSocket = serverSocket.accept();
+            count++;
+            System.out.println("client accepted " + count);
+            OutputStreamWriter writer = new OutputStreamWriter(clientSocket.getOutputStream());
+            writer.write("<h2>Hello server</h2>");
+            writer.flush();
+            writer.close();
+        }
+        //clientSocket.close();
 
     }
 }
