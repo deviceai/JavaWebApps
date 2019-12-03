cc0a0ff0b83d9fa38a63266f01bb8400e3952f4b
deviceai <device.ai@gmail.com>
2019-12-03 23:19:45 +0200

    simple client server 2 - with try catch

--- simpleClientServer2/src/Server.java
+++ simpleClientServer2/src/Server.java
@@ -3,23 +3,28 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Server {
-    public static void main(String[] args) throws IOException {  //Exception if binded port is busy
-        ServerSocket server = new ServerSocket(7000);
-        System.out.println("Server started");
-        Socket socket = server.accept();
-        System.out.println("Client connected");
+    public static void main(String[] args) {
 
-        OutputStream stream = socket.getOutputStream();
-        OutputStreamWriter streamWriter = new OutputStreamWriter(stream);
-        BufferedWriter writer = new BufferedWriter(streamWriter);
+        try ( ServerSocket server = new ServerSocket(7000))
+        {
+            System.out.println("Server started");
 
-        writer.write("Hello from server!");
-        writer.newLine();
-        writer.flush();
+            try (
+                    Socket socket = server.accept();
+                    OutputStream stream = socket.getOutputStream();
+                    OutputStreamWriter streamWriter = new OutputStreamWriter(stream);
+                    BufferedWriter writer = new BufferedWriter(streamWriter);
+                ){
+                    writer.write("Hello from server!");
+                    writer.newLine();
+                    writer.flush();
+                }
+        }
 
-        writer.close();
-        socket.close();
-        server.close();
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+
 
     }
 }
