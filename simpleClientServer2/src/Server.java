e5f2e01828927550e7d450db67eeefd47c47d547
deviceai <device.ai@gmail.com>
2019-12-03 23:55:57 +0200

    simple client server 2 - HTTP/1.1

--- simpleClientServer2/src/Server.java
+++ simpleClientServer2/src/Server.java
@@ -9,16 +9,31 @@ public class Server {
         {
             System.out.println("Server started");
 
-            try (
-                    Socket socket = server.accept();
-                    OutputStream stream = socket.getOutputStream();
-                    OutputStreamWriter streamWriter = new OutputStreamWriter(stream);
-                    BufferedWriter writer = new BufferedWriter(streamWriter);
-                ){
-                    writer.write("Hello from server!");
+            while(true) {
+                try (
+                        Socket socket = server.accept();
+                        OutputStream stream = socket.getOutputStream();
+                        OutputStreamWriter streamWriter = new OutputStreamWriter(stream);
+                        BufferedWriter writer = new BufferedWriter(streamWriter);
+
+                        InputStream InStream = socket.getInputStream();
+                        InputStreamReader InStreamReader = new InputStreamReader(InStream);
+                        BufferedReader reader = new BufferedReader(InStreamReader);
+
+
+                ) {
+                    //String request = reader.readLine();
+                    //System.out.println(request);
+                    while (!reader.readLine().isEmpty())
+                        System.out.println(reader.readLine());
+
+                    writer.write("HTTP/1.0 200 OK\n");
+                    writer.write("Content-Type: text/html\n");
                     writer.newLine();
+                    writer.write("<h1>TEST2</h1>\n");
                     writer.flush();
                 }
+            }
         }
 
         catch (IOException e) {
