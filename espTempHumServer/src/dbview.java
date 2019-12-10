4ef99b6cecb688eec71f3c6b6cb9cc172dd92ec4
deviceai <device.ai@gmail.com>
2019-12-10 12:33:58 +0200

    server base for esp8266 temp/hum logger

---
+++ espTempHumServer/src/dbview.java
@@ -0,0 +1,74 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.*;
+import static configs.config.DB_PASSWORD;
+import static configs.config.DB_USER;
+
+public class dbview extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request,
+                          javax.servlet.http.HttpServletResponse response)
+                            throws javax.servlet.ServletException, IOException {
+
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request,
+                         javax.servlet.http.HttpServletResponse response)
+                        throws javax.servlet.ServletException, IOException {
+
+        final String URL = "jdbc:mysql://localhost:3306/esptest";
+        final String USER = DB_USER;
+        final String PASS = DB_PASSWORD;
+
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        String title = "Database Results:";
+
+        String docType =
+                "<!doctype html public \"-//w3c//dtd html 4.0 " + "transitional//en\">\n";
+
+        out.println(docType +
+                "<html>\n" +
+                "<head><title>" + title + "</title></head>\n" +
+                "<body><h2>" + title + "</h2>\n");
+
+        //Connect to database
+        Connection connection = null;
+        try {
+            Class.forName("com.mysql.jdbc.Driver");  //Register DB driver
+            connection = DriverManager.getConnection(URL,USER,PASS);
+            //System.out.println(!connection.isClosed() ? "Connected!" : "Not connected!" );  //For debug
+
+            Statement stmt = connection.createStatement();
+            String sql = "SELECT id, temp, humidity, esp_status FROM esp8266";
+            ResultSet rs = stmt.executeQuery(sql);
+
+            // Extract data from result set
+            while(rs.next()){
+                //Retrieve by column name
+                int id  = rs.getInt("id");
+                int temp = rs.getInt("temp");
+                int humidity = rs.getInt("humidity");
+                String esp_status = rs.getString("esp_status");
+
+                //Display values
+                out.println("ID: " + id);
+                out.println(", Temp: " + temp);
+                out.println(", Humidity: " + humidity);
+                out.println(", Status: " + esp_status + "<br>");
+            }
+
+            out.println("<p></p>");
+            out.println("<a href=\"/\">back to main page</a>");
+            out.println("</body></html>");
+            rs.close();
+            stmt.close();
+            connection.close();
+
+        } catch (SQLException | ClassNotFoundException e) {
+            e.printStackTrace();
+
+        }
+
+
+    }
+}
---
+++ espTempHumServer/web/index.jsp
@@ -0,0 +1,18 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: aleksandralba
+  Date: 09/12/2019
+  Time: 16:06
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>$Title$</title>
+  </head>
+  <body>
+  <h2>ESP APP Main page</h2>
+  <a href="/dbview">Database view page</br></a>
+  <a href="/dbadmin">Database admin page</a>
+  </body>
+</html>
